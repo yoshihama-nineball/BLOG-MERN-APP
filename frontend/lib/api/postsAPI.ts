126ad106c4f901127257f2347034617784aa98c7
ezu0 <burimofumofu@gmail.com>
2025-02-20 17:54:19 +0900

    fix: backend

---
+++ frontend/lib/api/postsAPI.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:5000/api/v1/posts'
+
+// 型定義
+interface PostData {
+  postId?: string
+  title: string
+  description: string
+}
+
+//! Create post API
+export const createPostAPI = async (postData: PostData): Promise<any> => {
+  console.log(postData)
+  const response = await axios.post(`${BASE_URL}/create`, {
+    title: postData.title,
+    description: postData.description,
+  })
+  return response.data
+}
+
+// //! Update post API
+// export const updatePostAPI = async (postData: PostData): Promise<any> => {
+//   console.log(postData)
+//   const response = await axios.put(`${BASE_URL}/${postData.postId}`, {
+//     title: postData.title,
+//     description: postData.description,
+//   })
+//   return response.data
+// }
+
+// //! Fetch all posts
+// export const fetchAllPosts = async (): Promise<any> => {
+//   const response = await axios.get(BASE_URL)
+//   return response.data
+// }
+
+// //! Fetch post
+// export const fetchPost = async (postId: string): Promise<any> => {
+//   const response = await axios.get(`${BASE_URL}/${postId}`)
+//   return response.data
+// }
+
+// //! Delete post API
+// export const deletePostAPI = async (postId: string): Promise<any> => {
+//   const response = await axios.delete(`${BASE_URL}/${postId}`)
+//   return response.data
+// }
