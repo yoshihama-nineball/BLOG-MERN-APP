7e78b9c25ae449392b7df08fe1e17dce34f3f1b2
ezu0 <burimofumofu@gmail.com>
2025-03-02 19:00:06 +0900

    eslintrcの修正

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,42 +1,15 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'next/core-web-vitals',
-  ],
-  plugins: [
-    'react',
-    'react-hooks',
-    'jsx-a11y',
-    '@typescript-eslint',
-    'prettier',
-  ],
+  extends: ['next/core-web-vitals'],
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    'prettier/prettier': [
+      'warn', // errorからwarnに変更
+      {
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        printWidth: 80,
+        trailingComma: 'es5',
+      },
     ],
-    '@typescript-eslint/no-explicit-any': 'off', // any 型の警告を無効化
-    'jsx-a11y/no-autofocus': 'off', // autoFocus の警告を無効化
-    // 他のルールを追加する場合はここに記述
-    'no-unused-vars': 'off', // @typescript-eslint/no-unused-vars に統一
   },
 }
