8e90adef07f515bf9bc466b459b9be6e38c25c42
ezu0 <burimofumofu@gmail.com>
2025-03-03 10:22:27 +0900

    fix: eslint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,13 +1,19 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  rules: {
-    // カスタムルールを追加可能
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'no-console': 'warn'
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
   }
-};
+)
