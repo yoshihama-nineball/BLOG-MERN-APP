f79eaa8ad767f1dbc11ab1bcc19e2a2f7f84eecc
ezu0 <burimofumofu@gmail.com>
2025-03-14 22:36:43 +0900

    add: footer componentsのスタイル

--- frontend/app/components/layouts/Footer/Footer.tsx
+++ frontend/app/components/layouts/Footer/Footer.tsx
@@ -1,7 +1,36 @@
+'use client'
+import {
+  Box,
+  Container,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
 
 const Footer = () => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
   return (
-    <div>Footer</div>
+    <>
+      {!isMobile && (
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+          }}
+        >
+          <Container maxWidth="lg">
+            <Typography variant="body2" color="text.secondary" align="center">
+              © {new Date().getFullYear()} BLOG MERN APP -
+              MERNのブログプラットフォームアプリ
+            </Typography>
+          </Container>
+        </Box>
+      )}
+    </>
   )
 }
 
