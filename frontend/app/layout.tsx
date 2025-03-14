40649e95d29aba8764fc0fc4bf6180974c2bfb55
ezu0 <burimofumofu@gmail.com>
2025-03-14 15:27:42 +0900

    update: app/layoutにfooter追加追加

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,12 +3,14 @@ import { Metadata } from 'next'
 import React from 'react'
 import Loading from './components/feedback/Loading/Loading'
 import { ClientThemeProvider } from './components/layouts/ClientThemeProvider'
+import Footer from './components/layouts/Footer/Footer'
 import Header from './components/layouts/Header/Header'
 
 export const metadata: Metadata = {
   title: 'BLOG MERN APP | TOP',
   description: 'TOPページです',
 }
+
 
 
 export default function RootLayout({
@@ -22,6 +24,7 @@ export default function RootLayout({
         <ClientThemeProvider>
           <Header />
           <React.Suspense fallback={<Loading />}>{children}</React.Suspense>
+          <Footer />
         </ClientThemeProvider>
       </body>
     </html>
